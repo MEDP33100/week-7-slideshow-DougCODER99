3d2eba26ebf42890041ca40d563b80eeef67e578
Dora Do <doradocodes@gmail.com>
2025-04-11 10:24:53 -0400

    fixed my errors

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const dogImage = document.getElementById("dogImage");
 const prevBtn = document.getElementById("prevBtn");
-const nextBtn - document.getElemntById("nextBtn");
+const nextBtn = document.getElemntById("nextBtn");
 
 
 let currentImageIndex = 0;
@@ -8,8 +8,8 @@ let currentImageIndex = 0;
 let dogImages = [];
 
 
-asynch function fetchDogImages() {
-  const response= await fetch('https://api.thedogapi.com/v1/images/search?limit=5);
+async function fetchDogImages() {
+  const response= await fetch('https://api.thedogapi.com/v1/images/search?limit=5');
   const data = await response.json();
   dogImages = data.map(item => item.url);
   updateImage();
@@ -22,6 +22,7 @@ function showPrevImage() {
   } else {
 currentImageIndex = dogImages.length - 1;
   }
+}
 
 
 prevBtn.addEventListener("click", showPrevImage);
